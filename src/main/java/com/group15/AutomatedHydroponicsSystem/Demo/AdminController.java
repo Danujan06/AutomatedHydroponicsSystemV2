244e298da6105308bbcdf9c467449ffc42a407de
Danujan <sivasundaralingamdanujan@gmail.com>
2024-01-27 20:36:46 +0530

    Put and delete mapping added to the admin controller

--- src/main/java/com/group15/AutomatedHydroponicsSystem/Demo/AdminController.java
+++ src/main/java/com/group15/AutomatedHydroponicsSystem/Demo/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/admin")
 @PreAuthorize("hasRole('ADMIN')")
+@CrossOrigin
 public class AdminController {
 
     @GetMapping
@@ -20,5 +21,13 @@ public class AdminController {
         return "Post:: admin control";
     }
 
+    @PutMapping
+    public String put(){
+        return "PUT:: admin controller";
+    }
 
+    @DeleteMapping
+    public String delete(){
+        return "DELETE:: admin controller";
+    }
 }
